3cd045f20235f3cfe08310fd8f044c431dff21dc
Shehab <shetostito10@gmail.com>
2023-11-05 15:45:50 +0200

    adding web data model. closes #2

---
+++ beer-service-demo/src/main/java/com/example/beerservicedemo/web/dto/BeerDTO.java
@@ -0,0 +1,33 @@
+package com.example.beerservicedemo.web.dto;
+
+import com.example.beerservicedemo.web.entity.BeerStyleEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerDTO {
+
+    UUID id;
+    Integer version;
+    private OffsetDateTime createData;
+    private OffsetDateTime lastModifiedDate;
+    String beerName;
+    private BeerStyleEnum beerStyle;
+
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+
+}
---
+++ beer-service-demo/src/main/java/com/example/beerservicedemo/web/entity/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.example.beerservicedemo.web.entity;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
